c901694548dda6586757e00d1615f7f3ebf8df48
Carlos Rivera <carlosrdev@gmail.com>
2019-02-13 11:48:01 -0600

    Add files via upload

---
+++ TypeScript/HolaMundo/funciones.js
@@ -0,0 +1,13 @@
+"use strict";
+var saludo = "Hola soy una variable";
+var numero;
+numero = 211;
+var cualquiera;
+cualquiera = "hola";
+cualquiera = 2;
+function saludar() {
+    console.log(saludo);
+    console.log(numero);
+    console.log(cualquiera);
+}
+saludar();
---
+++ TypeScript/HolaMundo/funciones.ts
@@ -0,0 +1,14 @@
+let saludo="Hola soy una variable";
+let numero:number;
+numero=211;
+let cualquiera:any;
+cualquiera="hola";
+cualquiera=2;
+
+function saludar(){
+    console.log(saludo);
+    console.log(numero);
+    console.log(cualquiera);
+}
+
+saludar();
